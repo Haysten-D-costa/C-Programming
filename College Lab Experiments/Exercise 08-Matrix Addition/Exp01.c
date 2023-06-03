af0574560ec48076f722f7ccc89a2a95431bc5a3
Haysten Dcosta <haystendc@gmail.com>
2023-06-03 12:42:00 +0530

    Program Formatted

--- College Lab Experiments/Exercise 08-Matrix Addition/Exp01.c
+++ College Lab Experiments/Exercise 08-Matrix Addition/Exp01.c
@@ -1,81 +1,61 @@
-/* *****************************************************************************************************************
-Lab ID        : 8.1
-Program Title : Basics of C Programming
-Author        : Haysten D'costa
-Roll No.      : 21co56
-Class         : Comp B[Batch C3]
-Language      : C
-Due Date      : --
---------------------------------------------------------------------------------------------------------------------
-Description   : C Program to find the sum  of two matrices....
-Input         : two matrices
-Output        : resultant, sum martix
-Algorithm     : --
-Prerequisites : Basics of C
-Known Bugs    : NONE
-***************************************************************************************************************** */
-#include<stdio.h>
+#include <stdio.h>
 
-int main() //Main function...
+int main()
 {
-    //DECLARATION OF MATRICES AND VARIABLES...
-    int A[5][5],B[5][5],C[5][5];
-    int r1,r2,c1,c2,i,j;
-    //INPUT OF VARIABLES[ORDERs]...
-    printf("\nEnter the order of matrix -A:\n");
-    scanf("%d%d",&r1,&c1);
-    printf("\nEnter order of matrix -B:\n");
-    scanf("%d%d",&r2,&c2);
-    //IMP.CONDITION FOR ADDITION...
-    if((r1==r2)&&(c1==c2))
+    int A[5][5], B[5][5], C[5][5];
+    int r1, r2, c1, c2, i, j;
+
+    printf("\nEnter the order of matrix-A : ");
+    scanf("%d%d", &r1, &c1);
+    printf("Enter the order of matrix-B : ");
+    scanf("%d%d", &r2, &c2);
+    if ((r1 == r2) && (c1 == c2))
     {
-        printf("\nEnter the elements of matrix -A:\n");
-        for(i=0;i<r1;i++)
-            for(j=0;j<c1;j++)
-                scanf("%d",&A[i][j]);
-        printf("\nEnter the elements of matrix -B:\n");
-        for(i=0;i<r2;i++)
-            for(j=0;j<c2;j++)
-                scanf("%d",&B[i][j]);
-        //OUTPUT OF USER INPUTED MATRICES...
+        printf("\nEnter the elements of matrix-A : ");
+        for (i = 0; i < r1; i++)
+            for (j = 0; j < c1; j++)
+                scanf("%d", &A[i][j]);
+        printf("\nEnter the elements of matrix-B : ");
+        for (i = 0; i < r2; i++)
+            for (j = 0; j < c2; j++)
+                scanf("%d", &B[i][j]);
+        // OUTPUT OF USER INPUTED MATRICES...
         printf("\nMatrix A entered is :\n");
-        for(i=0;i<r1;i++)
+        for (i = 0; i < r1; i++)
         {
-            for(j=0;j<c1;j++)
+            for (j = 0; j < c1; j++)
             {
-                printf("%d\t",A[i][j]);
+                printf("%d\t", A[i][j]);
             }
             printf("\n");
         }
         printf("\nMatrix B entered is :\n");
-        for(i=0;i<r2;i++)
+        for (i = 0; i < r2; i++)
         {
-            for(j=0;j<c2;j++)
+            for (j = 0; j < c2; j++)
             {
-                printf("%d\t",B[i][j]);
+                printf("%d\t", B[i][j]);
             }
             printf("\n");
         }
-        //PROCESS OF SUM OF MATRICES A and B...
-        for(i=0;i<r2;i++)
+        for (i = 0; i < r2; i++)
         {
-            for(j=0;j<c2;j++)
+            for (j = 0; j < c2; j++)
             {
-                C[i][j]=A[i][j]+B[i][j];
+                C[i][j] = A[i][j] - B[i][j];
             }
         }
-        //OUTPUT OF THE RESULTANT[SUM MATRIX-C]...
-        printf("\nThe resultant sum matrix is :\n");
-        for(i=0;i<r1;i++)
+        printf("\nThe resultant matrix (A - B) is :\n");
+        for (i = 0; i < r1; i++)
         {
-            for(j=0;j<c1;j++)
+            for (j = 0; j < c1; j++)
             {
-                printf("%d\t",C[i][j]);
+                printf("%d\t", C[i][j]);
             }
             printf("\n");
         }
     }
     else
-         printf("\nERROR: MATRIX ADDITION NOT POSSIBLE \n....check order entered....");
-    return(0);
+        printf("\nInvalid orders entered....");
+    return (0);
 }
